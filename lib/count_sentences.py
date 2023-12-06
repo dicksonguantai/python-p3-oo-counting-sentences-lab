f03a3b87802ef4378fc82b15174a164a95fa2569
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-12-06 23:51:31 +0300

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
+import re
+class MyString :
+    def __init__(self,value=""):
+        if  not isinstance(value, str):
+            print("The value must be a string.")
+        else:
+         self.value =value
 
-class MyString:
-  pass
+    
+    def is_sentence(self):
+        return self.value.endswith(".")
+    
+    def is_question(self):
+        return self.value.endswith("?")
+    
+    def is_exclamation(self):
+        return self.value.endswith("!")
+    
+    def count_sentences(self):
+        sentence = [sentence.strip() for sentence in re.split(r'[.!?]', self.value) if sentence.strip()]
+        return len(sentence)
+        
+
+
